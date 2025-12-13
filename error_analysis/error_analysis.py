2ad0ebafc4974b61653009fc22dc9aa95eb16f3b
Leon Meyer <meyer@lmeyer.de>
2025-12-13 16:17:26 +0100

    added code from marius: error analysis and baseline model predictions

--- error_analysis/error_analysis.py
+++ error_analysis/error_analysis.py
@@ -4,6 +4,8 @@ import seaborn as sns
 
 import sys
 
+from collections import Counter
+
 def main():
     df_predicted = pd.read_csv("val_predictions.csv")
     df_predicted.columns = ["image_path","label"]
@@ -14,13 +16,39 @@ def main():
     df_label_dist = pd.read_csv("label_dist.csv")
     df_merged = df_merged.merge(df_label_dist, left_on = "label_gt", right_on = "label")
 
+    df_baseline = pd.read_csv("baseline_predictions.csv")
+    df_own_model = pd.read_csv("predictions_own_model.csv")
+
+    df_models_merged = df_own_model.merge(df_baseline, on="id", suffixes=["_own","_base"])
+    df_models_merged["agreement"] = df_models_merged["label_own"] == df_models_merged["label_base"]
+    print(df_models_merged.head())
+
     accuracy_per_class = (
-    df_merged
-    .groupby("label_gt")["correct"]
+    df_models_merged
+    .groupby("label_base")["agreement"]
     .mean()
     .mul(100)
-    .reset_index(name="accuracy_pct")
+    .reset_index(name="agreement_pct")
     )
-    print(accuracy_per_class)
+    accuracy_per_class = accuracy_per_class.merge(df_label_dist, left_on="label_base", right_on="label")
+    df_models_merged_error = df_models_merged[df_models_merged["agreement"] == False]
+    c=Counter(df_models_merged_error[["label_own", "label_base"]])
+    print(c.most_common())
+    sys.exit()
+
+
+
+
+
+    plt.figure(figsize=(8,4.5), dpi=300)
+    sns.regplot(accuracy_per_class, x="num_images", y="agreement_pct", scatter=False,
+    lowess=True,
+    color="#0797df",
+    line_kws={'linewidth':10})
+    #plt.title("Agreement vs. image count in train set", fontweight="bold")
+    plt.xlabel("Number of images in train set", fontsize=15, fontweight="bold")
+    plt.ylabel("Agreement baseline & own model", fontsize=15, fontweight="bold")
+    #plt.show()
+    plt.savefig("agreement_class_size.png")
 if __name__ == '__main__':
     main()
